92c968ffd1ed2bf0ab7801d0ef89ef7b9d2221d3
Haczyn <60531623+Haczyn@users.noreply.github.com>
2022-04-05 18:27:23 +0200

    powershell-liveid

--- Office365 rername user/RENAME USE AFTER SYNC.ps1
+++ Office365 rername user/RENAME USE AFTER SYNC.ps1
@@ -1,1 +1,16 @@
-f
+$username = "officelogin"
+$password = "officepwd"
+
+$secureStringPwd = $password | ConvertTo-SecureString -AsPlainText -Force 
+$UserCredential = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $secureStringPwd
+
+
+Connect-MsolService -Credential $UserCredential
+
+Get-PSSession | Remove-PSSession
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic –AllowRedirection
+Import-PSSession $Session
+
+
+
+set-msoluserprincipalname -newuserprincipalname new.name@domian.com -userprincipalname adlogonname@domianHQ.onmicrosoft.com
